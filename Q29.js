ab461a1104c7630d17251b4feb15ccfe5187c573
Gautam Singh <149056911+Darnxd@users.noreply.github.com>
2026-04-01 19:23:15 +0530

    Add files via upload

---
+++ Q29.js
@@ -0,0 +1,15 @@
+function calculator(a, b, operator){
+  
+  if(operator === "+"){
+    return a + b;
+  }
+  else if(operator === "-"){
+    return a - b;
+  }
+  else{
+    return "Invalid Operator";
+  }
+
+}
+
+console.log(calculator(10, 5, "+"));
